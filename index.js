35c15084cf89e601e4ec58b6eb7749a5a0306410
Jannis R <mail@jannisr.de>
2019-03-02 12:37:17 +0100

    implementation :zap:

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 'use strict'
 
-// todo
+const {DateTime} = require('luxon')
+
+const TIMEZONE = 'Europe/Berlin'
+const FRIDAY = 5 // Sunday is `0`
+const TIME = {
+	hour: 20, minute: 0, second: 0, millisecond: 0
+}
+
+function* computeNextEvents (t0 = Date.now()) {
+	let t = DateTime.fromMillis(t0, {zone: TIMEZONE})
+	.plus({week: 1})
+	.set({weekday: FRIDAY})
+	.set(TIME)
+
+	while (true) {
+		const nextFriday = t.plus({week: 1}).set(TIME)
+		if (nextFriday.month !== t.month) yield +t
+		t = nextFriday
+	}
+}
+
+module.exports = computeNextEvents
